c3e103794de1e5284d4869a5c3e72507b676f8b5
Claiton dos santos silva <101401437+Claitonok@users.noreply.github.com>
2023-10-02 20:50:48 -0300

    Update Cliente.php

--- Projeto01(formulario)/Cliente.php
+++ Projeto01(formulario)/Cliente.php
@@ -1,77 +1,87 @@
 <!DOCTYPE html>
 <html lang="pt-br">
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="ICON" type="ICON" sizes="16x16" href="./img/img.ico">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<link rel="ICON" type="ICON" sizes="16x16" href="./img/img.ico">
     <link rel="stylesheet" href="./cliente.css">
     <title>Bem vindo </title>
-  </head>
-<header>  
-    <h2><a href="./index.html">&#9776</a></h2>
-  <nav>
-  <ul>
-        <li><a href="./index.html">Home</a></li>
-    </ul>
-  </nav>
-  </header>
-  <figure>
+</head>
+<body>
+
+<!-- Top Navigation Menu -->
+<div class="topnav">
+<a href="#home" class="active">Cliente</a>
+  <div id="myLinks">
+    <a href="./index.html">Home</a>
+    <a href="./Contatos.html">Contato</a>
+    <a href="./sobre.html">Sobre</a>
+  </div>
+  <a href="javascript:void(0);" class="icon" onclick="myFunction()">
+    <i class="fa fa-bars"></i>
+  </a>
+</div>
+
+<script>
+function myFunction() {
+  var x = document.getElementById("myLinks");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
+</script>
+
+<figure>
     <img src="./img/paisagem.jpg" alt="">
     <figcaption><b> 1. Imagem</b></figcaption>
   </figure>
+
+
   <body>
     <section>
-    <h1>BEM VINDO</h1>
+    <h1>BEM VINDO </h1>
   </section>
- 
+
   <?php
+
    $nome = $_POST["nome"];
    $email = $_POST["email"];
    $file = $_POST["file"];
    $datetime = date('d/m/Y');
-  
+
    $to      = $email;
    $subject = 'the subject';
    $message = 'hello';
    $headers = array(
-    'From' => 'webmaster@example.com',
-    'Reply-To' => 'webmaster@example.com',
-    'X-Mailer' => 'PHP/' . phpversion()
-);
+       'From' => $email,
+       'Reply-To' => $email,
+       'X-Mailer' => 'PHP/' . phpversion()
+   );
+   
+   mail($to, $subject, $message, $headers);
 
-mail($to, $subject, $message, $headers);
    ?>
-<!---   
-      Reparo no erro do envio de email -- envio via link Direto
-
-<address>
-Written by <a href="mailto:webmaster@example.com">Jon Doe</a>.<br> 
-Visit us at:<br>
-Example.com<br>
-Box 564, Disneyland<br>
-USA
-</address>
-      >
 
 <form>
     <fieldset>
 
       <legend>Cliente</legend>
     <p>Nome : <?= $nome ?></p>
-    <p>Email:  <?=$email?></p>
+    <p>Email: <?=$email?></p>
     <p>File : <?= $file ?></p>
     <p>Data : <?= $datetime ?></p><br>
     <br>
     <br>
     <br>
-    <button type="submit" id="button" value="Enviar">Enviar</button><br>
+    <button id="button" value="Enviar">Enviar</button><br>
     <br>
+
   </fieldset>
     <br>
     
   </form>
+
 </body>
 </html>
-
-   
